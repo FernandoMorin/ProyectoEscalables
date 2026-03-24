267cfa039bf17b526535bd86ccf2b2a0bad90382
FernandoMorin <a357976@alumnos.uaslp.mx>
2026-03-24 14:03:54 -0600

    Ya jalo el fondo wuuu

--- src/app/pages/home/hero/hero.ts
+++ src/app/pages/home/hero/hero.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
   imports: [],
   templateUrl: './hero.html',
-  styleUrl: './hero.css',
+  styleUrl: './hero.css'
 })
-export class Hero {
+export class Hero implements AfterViewInit {
+  @ViewChild('heroVideo') heroVideo!: ElementRef<HTMLVideoElement>;
 
+  ngAfterViewInit(): void {
+    const video = this.heroVideo?.nativeElement;
+
+    if (!video) return;
+
+    video.addEventListener('ended', () => {
+      video.pause();
+      video.currentTime = video.duration;
+    });
+  }
 }
