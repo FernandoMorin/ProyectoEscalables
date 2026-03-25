7d4b366c75015f70f34ee7686fe809d5fad25541
FernandoMorin <a357976@alumnos.uaslp.mx>
2026-03-24 18:11:16 -0600

    Ya ta el about

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../core/navbar/navbar';
 import { Hero } from './hero/hero';
+import { About } from './about/about';
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar, Hero],
+  imports: [Navbar, Hero, About],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
