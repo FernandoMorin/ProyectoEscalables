dd9c0f5b6f0b2047359620d7a15127edb320e4c3
FernandoMorin <a357976@alumnos.uaslp.mx>
2026-03-24 18:20:04 -0600

    Ya tan los servicios

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Navbar } from '../../core/navbar/navbar';
 import { Hero } from './hero/hero';
 import { About } from './about/about';
+import { Services } from './services/services';
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar, Hero, About],
+  imports: [Navbar, Hero, About, Services],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
